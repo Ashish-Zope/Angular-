d10c0730798fe329946e7f45a2fc6e68063fba07
Ashish <ashish.zope93@gmail.com.com>
2020-08-01 17:12:57 +0530

    setup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipieDetailsComponent } from './recipies/recipie-details/recipie-details.component';
+import { RecipieItemComponent } from './recipies/recipies-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipieDetailsComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,FormsModule
